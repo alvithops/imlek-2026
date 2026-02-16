d9ad2422bd8543b4f19b4085751a43ca590aadad
alvithops <alvithops@gmail.com>
2026-02-16 21:28:04 +0700

    edit 4

--- public/imlek-2026/script.js
+++ public/imlek-2026/script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  // usaha pertama langsu
+  // usaha pertama langs
   playAudio();
 
   // usaha tambahan: setelah interaksi ringan (sentuhan/klik dimana saja, tapi tanpa mengganggu UI)
