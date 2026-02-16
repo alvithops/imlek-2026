e0b649e09e4aed21e6b4f9efaa004cd37d6fa49e
alvithops <alvithops@gmail.com>
2026-02-16 22:53:04 +0700

    edit 138

--- public/imlek-2026/script.js
+++ public/imlek-2026/script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  // usaha  pertama langsung sekarang mancanegara kertanegara wanadakusukmakalanan
+  // usaha  pertama langsung sekarang mancanegara kertanegara wanadakusukmakalana
   playAudio();
 
   // usaha tambahan: setelah interaksi ringan (sentuhan/klik dimana saja, tapi tanpa mengganggu UI)
