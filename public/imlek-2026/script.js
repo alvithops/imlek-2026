76416097ec5a757f52c5b2e472c0f775636d8643
alvithops <alvithops@gmail.com>
2026-02-16 22:26:25 +0700

    edit 73

--- public/imlek-2026/script.js
+++ public/imlek-2026/script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  // usaha  pertama langsung sekarang mancanegara kertanee
+  // usaha  pertama langsung sekarang mancanegara kertane
   playAudio();
 
   // usaha tambahan: setelah interaksi ringan (sentuhan/klik dimana saja, tapi tanpa mengganggu UI)
